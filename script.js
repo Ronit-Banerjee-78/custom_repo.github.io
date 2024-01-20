44fd4d2479b68fdbf8924b9cc8ed51e613d6fe2d
Ronit Banerjee <103939129+Ronit-Banerjee-78@users.noreply.github.com>
2024-01-20 22:02:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
+const APIURL="https://api.github.com/users/"
 
-  const APIURL="https://api.github.com/users/"
 
 const getUser=async(username)=>{
       const res=await fetch(`${APIURL+username}/repos`);
       const data=await res.json();
 
-      console.log(data);
+console.log(data);
 
+
+
+let itemperPage=10;
+let curentPAge=1;
+  const repoLength=data.length;
+  const pages=[];
+
+  for(let i=0;i<=Math.ceil(repoLength/itemperPage);i++){
+    pages.push(i)
+  }
+
+  const indexofLastPage=curentPAge*itemperPage;
+  const indexofFisrtPage=indexofLastPage-itemperPage;
+  const currentItems=data.slice(indexofFisrtPage,indexofLastPage);
+console.log(currentItems);
+console.log(indexofFisrtPage);
+console.log(indexofLastPage);
+//showing data
 const rowHap=document.querySelector('.row')
 
 const cardsShow=()=>{
-  data.map((user)=>{
+  currentItems.map((user)=>{
     const postData=document.createElement("div")
-    // rowHap.classList.add('.row');
+    postData.classList.add('.card');
     postData.innerHTML=`
     <div class="card col-6" style="width: 35rem;">
      <div class="card-body">
-     <h5 class="card-title">{user.name}</h5>
-     <p class="card-text">{user.description}.</p>
-     <a href="#" class="btn btn-primary">{user.language}</a>
+     <h5 class="card-title">${user.name}</h5>
+     <p class="card-text">${user.description}.</p>
+     <a href="#" class="btn btn-primary">${user.language}</a>
      </div>
      </div>
     `
@@ -30,43 +48,23 @@ rowHap.appendChild(postData);
 }
 cardsShow();
 
-
-// let tab='';
-// data.forEach(function(user){
-//   tab +=`
-//   <div class="card col-6" style="width: 35rem;">
-//   <div class="card-body">
-//     <h5 class="card-title">${user.name}</h5>
-//     <p class="card-text">${user.description}.</p>
-//     <a href="#" class="btn btn-primary">${user.language}</a>
-//   </div>
-//   </div>`
-// })
-
+const prevBtn=()=>{
+  if((curentPAge-1)*itemperPage){
+    curentPAge=curentPAge-1
+    // cardsShow();
+    console.log(currentItems);
+  }
+}
+const nxtBtn=()=>{
+  if((curentPAge*itemperPage)/repoLength){
+    curentPAge=curentPAge+1;
+    // cardsShow();
+    console.log(currentItems);
 
   }
-
-  getUser("johnpapa");
-
-
-
-
-  {/* <div class="card col-6" style="width: 35rem;">
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-  </div>
-  <div class="card col-6" style="width: 35rem;">
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-  </div> */}
-
+}
+document.getElementById("prev").addEventListener("click",prevBtn);
+document.getElementById("next").addEventListener("click",nxtBtn);
+}
+getUser("johnpapa");
 
